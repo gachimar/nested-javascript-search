ab88d9c5b744791af4c9d8b83a5dcae1680d2349
gachimar <gabrielchirinosmarcano@gmail.com>
2024-07-16 16:05:20 -0400

    Test added, name changed, typescript

--- dist/index.js
+++ dist/index.js
@@ -1,127 +1,119 @@
-import { escape } from "./utils/regexescaper";
-
+/**
+     * Function to prevent unexpected regex expressions from the required string.
+     *
+     * @param {String} str
+     * @returns { String }
+     */
+export function escape(str = '') {
+    return str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
 
 /**
      * Funcion principal de busqueda.
-     * 
+     *
      * @param {Array} json          // Array a investigar.
      * @param {String} parametro    // Cadena de texto a evaluar con los elementos.
      * @param {Array} blacklist     // Nombre de las keys a ignorar.
      * @returns { Array }
      */
-export function search (json : Array<any>, parametro : string, blacklist : Array<any> = []){
-    const scapedParam =  escape(parametro);
-    const parametroRegex : RegExp = new RegExp(scapedParam, "gi")
-    const arrayFiltrada = this.searcher(json, parametroRegex, blacklist)
-    return arrayFiltrada
-} 
-
+export function search(json, parametro, blacklist = []) {
+    const scapedParam = escape(parametro);
+    const parametroRegex = new RegExp(scapedParam, "gi");
+    const arrayFiltrada = searcher(json, parametroRegex, blacklist);
+    return arrayFiltrada;
+}
 // Json es cualquier json o array clave valor (Funciona sin clave, genial),
 // parametro debe ser una expresión regular a evaluar.
 /**
  * Function to iterate in the given array and look for matches in the key values.
- * 
+ *
  * @param {Array} json          // Array a investigar.
  * @param {String} parametro    // Cadena de texto a evaluar con los elementos.
  * @param {Array} blacklist     // Nombre de las keys a ignorar.
- * @returns 
+ * @returns
  */
-export function searcher(json : Array<any>, parametro : RegExp, blacklist : Array<any>){
-    
-    
+export function searcher(json, parametro, blacklist) {
     // La voy a llenar de los elementos del array los cuales al menos
     // en sus elementos o elementos anidados hay una coincidencia.
-    const filterBySearchArray : Array<any> = []
-
+    const filterBySearchArray = [];
     // Recorrer todos los elementos del array del json
     try {
         json.forEach((element) => {
-
             // Esto es por si no es clave valor, verifique inmediatamente
             // el valor del elemento actual del array, si encuentra un
             // numero o string, verificar match.
-            if ( ['string', 'number'].includes(typeof element) ){
-                const hallado = String(element).match(parametro)
+            if (['string', 'number'].includes(typeof element)) {
+                const hallado = String(element).match(parametro);
                 if (hallado !== null) {
-                    filterBySearchArray.push(element)
-                    return
+                    filterBySearchArray.push(element);
+                    return;
                 }
             }
-
             // Si es clave valor y encuentra un numero o string, verificar match.
-            if ( ['string', 'number'].includes(typeof element) ) {
-                const hallado = String(element).match(parametro)
-                
+            if (['string', 'number'].includes(typeof element)) {
+                const hallado = String(element).match(parametro);
                 if (hallado !== null) {
-                    filterBySearchArray.push(element)
-                    return
+                    filterBySearchArray.push(element);
+                    return;
                 }
-            } 
-
+            }
             // Si no es lo anterior y es un array, entonces llamar de nuevo
             // en la funcion si hay mas elementos dentro del array.
             else if (Array.isArray(element)) {
-                const recursivo = this.searcher(element, parametro, blacklist);
+                const recursivo = searcher(element, parametro, blacklist);
                 if (recursivo.length > 0) {
-                    filterBySearchArray.push(element)
-                    return
+                    filterBySearchArray.push(element);
+                    return;
                 }
             }
-            
             // Recorrer todas las claves del objeto
-            if (typeof element == "object"){
+            if (typeof element == "object") {
                 for (const key in element) {
                     if (blacklist.includes(key)) {
-                        continue
+                        continue;
                     }
-                    
                     // Esto es por si no es clave valor, verifique inmediatamente
                     // el valor del elemento actual del array, si encuentra un
                     // numero o string, verificar match.
-                    if ( ['string', 'number'].includes(typeof element) ){
-                        const hallado = String(element).match(parametro)
+                    if (['string', 'number'].includes(typeof element)) {
+                        const hallado = String(element).match(parametro);
                         if (hallado !== null) {
-                            filterBySearchArray.push(element)
-                            return
+                            filterBySearchArray.push(element);
+                            return;
                         }
                     }
-    
                     // Si es clave valor y encuentra un numero o string, verificar match.
-                    if ( ['string', 'number'].includes(typeof element[key]) ) {
-                        const hallado = String(element[key]).match(parametro)
-                        
+                    if (['string', 'number'].includes(typeof element[key])) {
+                        const hallado = String(element[key]).match(parametro);
                         if (hallado !== null) {
-                            filterBySearchArray.push(element)
-                            return
+                            filterBySearchArray.push(element);
+                            return;
                         }
-                    } 
+                    }
                     // Si es clave valor y encuentra un numero o string, verificar match.
-                    if ( ['string', 'number'].includes(typeof element[key]) ) {
-                        const hallado = String(element[key]).match(parametro)
-                        
+                    if (['string', 'number'].includes(typeof element[key])) {
+                        const hallado = String(element[key]).match(parametro);
                         if (hallado !== null) {
-                            filterBySearchArray.push(element)
-                            return
+                            filterBySearchArray.push(element);
+                            return;
                         }
-                    } 
-
+                    }
                     // Si no es lo anterior y es un array, entonces llamar de nuevo
                     // en la funcion si hay mas elementos dentro del array.
                     else if (Array.isArray(element[key])) {
-                        const recursivo = this.searcher(element[key], parametro, blacklist);
+                        const recursivo = searcher(element[key], parametro, blacklist);
                         if (recursivo.length > 0) {
-                            filterBySearchArray.push(element)
-                            return
+                            filterBySearchArray.push(element);
+                            return;
                         }
                     }
-                }   
+                }
             }
-            
-        });  
-    } catch (error) {
-        console.log(error)
-        throw new Error('The given element is not an iterable array.') 
+        });
     }
-    
+    catch (error) {
+        console.log(error);
+        throw new Error('The given element is not an iterable array.');
+    }
     return filterBySearchArray;
 }
---
+++ tests/test.js
@@ -0,0 +1,24 @@
+import { search } from '../dist/index.js'
+
+const myArray = [
+    {
+        id:0,
+        name:'foo',
+        nested: {
+            name:'cake'
+        }
+    },
+    {
+        id:1,
+        name:'bar',
+        nestedArray:[
+            {name:'coffee'}
+        ]
+    },
+]
+
+console.log(search(myArray,'coffee'))   // [ { id: 1, name: 'bar', nestedArray: [ [Object] ] } ]
+
+console.log(search(myArray,'foo'))      // [ { id: 0, name: 'foo', nested: { name: 'cake' } } ]
+
+console.log(search(myArray,'cake'))      // [] This version only search inside arrays of objects.
